e55d3e89c59ce911cfe7dd8a2ee123ced18378ba
Yang Zhou <gavinzhou10c@gmail.com>
2016-02-12 13:48:19 -0500

    [Add] Change the comparator method of deck

--- module02/ca/mcgill/comp303/module02/lecture2/Deck.java
+++ module02/ca/mcgill/comp303/module02/lecture2/Deck.java
@@ -15,57 +15,97 @@ import ca.mcgill.comp303.module02.lecture2.Card.Suit;
 public class Deck implements Iterable<Card>
 {
 	private final Stack<Card> aCards = new Stack<>();
-	
+
 	public static void main(String[] args)
 	{
 		Deck deck = new Deck();
 		deck.shuffle();
 		deck.sort();
-		for( Card card : deck )
+		for (Card card : deck)
 		{
 			System.out.println(card);
 		}
 	}
-	
+
 	public Deck()
-	{}
-	
+	{
+	}
+
+	// public void sort()
+	// {
+	// Collections.sort(aCards, Card.createByRankComparator());
+	// }
+
+	// public void sort()
+	// {
+	// Collections.sort(aCards, new Comparator<Card>()
+	// {
+	//
+	// @Override
+	// public int compare(Card o1, Card o2)
+	// {
+	// if (o1.getRank() == o2.getRank())
+	// {
+	// return o1.getSuit().ordinal() - o2.getSuit().ordinal();
+	// }
+	// else
+	// {
+	// return o1.getRank().ordinal() - o2.getRank().ordinal();
+	// }
+	// }
+	// });
+	// }
+
 	public void sort()
 	{
-		Collections.sort(aCards, Card.createByRankComparator());
+		Collections.sort(aCards, new Comparator<Card>()
+		{
+			@Override
+			public int compare(Card o1, Card o2)
+			{
+				if (o1.getSuit() == o2.getSuit())
+				{
+					return o1.getRank().ordinal() - o2.getRank().ordinal();
+				}
+				else
+				{
+					return o1.getSuit().ordinal() - o2.getSuit().ordinal();
+				}
+			}
+		});
 	}
-	
+
 	public List<Card> getCards()
 	{
 		return Collections.unmodifiableList(aCards);
 	}
-	
-	public Deck( Deck pDeck )
+
+	public Deck(Deck pDeck)
 	{
-		for( Card card : pDeck.aCards )
+		for (Card card : pDeck.aCards)
 		{
 			aCards.add(new Card(card));
 		}
 	}
-	
+
 	public void shuffle()
 	{
 		aCards.clear();
-		for( Suit suit : Suit.values() )
+		for (Suit suit : Suit.values())
 		{
-			for( Rank rank : Rank.values())
+			for (Rank rank : Rank.values())
 			{
 				aCards.push(new Card(rank, suit));
 			}
 		}
 		Collections.shuffle(aCards);
 	}
-	
+
 	public boolean isEmpty()
 	{
 		return aCards.isEmpty();
 	}
-	
+
 	public Card draw()
 	{
 		return aCards.pop();
